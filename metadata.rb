17646e5a54c255e96d20dc77bb64a200966951be
Danny Guinther <dannyguinther@gmail.com>
2014-08-31 08:53:28 -0400

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name              'lib'
+maintainer        'Freewrite.org'
+maintainer_email  'dev@freewrite.org'
+license           'MIT'
+description       'Various helper libraries worth sharing'
+long_description  'Various helper libraries worth sharing; including credentials management and service detection.'
+version           '0.0.1'
+
+recipe 'lib', 'Installs helper libs into Chef::Recipe'
+
+supports 'debian'
+supports 'ubuntu'
